abcc9a932efa6dfa67894dd0e6ccef869a49f31c
Abdulsalam Opeyemi <opdort24@gmail.com>
2017-11-05 15:01:00 +0100

    Third commit

--- app/src/main/java/com/example/opeyemi/yourbitcoinconverter/MainActivity.java
+++ app/src/main/java/com/example/opeyemi/yourbitcoinconverter/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.opeyemi.yourbitcoinconverter;
 
 import android.app.LoaderManager;
-import android.content.Intent;
+import android.content.Context;
 import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v4.view.PagerTabStrip;
 import android.support.v4.view.ViewPager;
@@ -12,6 +14,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ProgressBar;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Card[]> {
     Spinner spinner; //spinner containing the list of all currencies
@@ -21,25 +24,45 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     ProgressBar progressBar;
     //pager tab strip holding the header of each fragment
     PagerTabStrip pagerTabStrip;
+    TextView emptyView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getLoaderManager().initLoader(1,null,this).forceLoad();
+
+
+        //check for suitable  connection before continuing to load the data into the field
+        ConnectivityManager cm = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+
+        if(activeNetwork != null && activeNetwork.isConnected()){
+            getLoaderManager().initLoader(1,null,this).forceLoad();
+        } else {
+            //Hide the progress bar
+            progressBar = (ProgressBar) findViewById(R.id.progressBar);
+            progressBar.setVisibility(View.GONE);
+
+
+            //Create an empty state textView to display if there is no internet connection
+            emptyView = (TextView) findViewById(R.id.no_values_text_view);
+            emptyView.setText("No internet connection");
+        }
+
 
         //create spinner - set adapter and set the item click listener
         spinner = (Spinner) findViewById(R.id.spinner);
         spinnerAdapter = new ArrayAdapter(this,R.layout.support_simple_spinner_dropdown_item,UtilityClass.currencies);
         spinner.setAdapter(spinnerAdapter);
 
+
+
     }
 
-    public void openNext(View view){
-        Intent intent = new Intent(this,ConvertActivity.class);
-        startActivity(intent);
-    }
+
 
     @Override
     public Loader<Card[]> onCreateLoader(int id, Bundle args) {
@@ -48,7 +71,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     @Override
     public void onLoadFinished(Loader<Card[]> loader, Card[] data) {
-        //Hide the progress bar and display the result found
+        //Hide the progress bar
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
         progressBar.setVisibility(View.GONE);
 
